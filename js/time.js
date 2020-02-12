f8af0282232c1c9dd51d5e00ace26c9e43dc18f0
wangqixing1761 <53212445+wangqixing1761@users.noreply.github.com>
2020-02-12 10:59:38 +0800

    Add files via upload

--- js/time.js
+++ js/time.js
@@ -1,6 +1,6 @@
 ﻿function showTime() { 
 	var currentDate = new Date(); 
-	var startDate = new Date(2019, 11, 24);
+	var startDate = new Date(2019, 10, 3);
 	var date3 = currentDate-startDate;
 	var days=Math.floor(date3/(24*3600*1000));
 	var leave1=date3%(24*3600*1000);     //计算天数后剩余的毫秒数
